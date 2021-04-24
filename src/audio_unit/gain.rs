635843e3555f9d5a61c3be460f4a6a6783ede9b5
Peter Stuart <peter@peterstuart.org>
2021-04-24 12:44:25 -0400

    Split Gain out from Delay. (#12)

---
+++ src/audio_unit/gain.rs
@@ -0,0 +1,21 @@
+use crate::{AudioUnit, Result};
+
+pub struct Gain {
+    gain: f32,
+}
+
+impl Gain {
+    pub fn new(gain: f32) -> Self {
+        Self { gain }
+    }
+}
+
+impl AudioUnit for Gain {
+    fn process(&mut self, input: &[f32], output: &mut [f32]) -> Result<()> {
+        for i in 0..input.len() {
+            output[i] = input[i] * self.gain;
+        }
+
+        Ok(())
+    }
+}
--- src/audio_unit/mod.rs
+++ src/audio_unit/mod.rs
@@ -1,21 +1,20 @@
 mod delay;
+mod gain;
 mod pipeline;
 mod split;
 mod transparent;
 
 pub use delay::Delay;
+pub use gain::Gain;
 pub use pipeline::Pipeline;
 pub use split::Split;
 pub use transparent::Transparent;
 
 use crate::Result;
-use std::fmt::Display;
 
 pub type Boxed = Box<dyn AudioUnit>;
 
 pub trait AudioUnit: Send + Sync {
-    fn name(&self) -> String;
-
     fn process(&mut self, input: &[f32], output: &mut [f32]) -> Result<()>;
 
     fn boxed(self) -> Boxed
@@ -23,11 +22,5 @@ pub trait AudioUnit: Send + Sync {
         Self: 'static + Sized,
     {
         Box::new(self)
-    }
-}
-
-impl Display for dyn AudioUnit {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Display::fmt(&self.name(), f)
     }
 }
